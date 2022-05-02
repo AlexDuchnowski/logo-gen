0408f07616b394d450c782cc6428e18e8a24891f
AlexDuchnowski <alex_duchnowski@brown.edu>
2022-05-01 21:38:31 -0400

    updated download.sh to include metadata

--- download.sh
+++ download.sh
@@ -8,3 +8,12 @@ unzip LLD-icon_PKL.zip
 rm LLD-icon_PKL.zip
 
 echo downloaded data
+
+#this downloads the zip file that contains the data
+curl https://data.vision.ee.ethz.ch/sagea/lld/data/LLD-logo_metadata.zip --output LLD-logo_metadata.zip
+# this unzips the zip file
+unzip LLD-logo_metadata.zip
+# this cleans up the zip file, as we will no longer use it
+rm LLD-logo_metadata.zip
+
+echo downloaded data
