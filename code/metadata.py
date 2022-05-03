aa8739795b8f490433f9048952285b4e6bc3d643
yuqinan <78807720+yuqinan@users.noreply.github.com>
2022-05-03 11:50:11 -0400

    Update metadata.py

--- code/metadata.py
+++ code/metadata.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import pickle as cPickle
 import pprint as pp
+pp = pprint.PrettyPrinter(indent=2)
 
 with open("../LLD-logo_metadata.pkl", 'rb') as f:
     metadata = cPickle.load(f, encoding='latin1')
