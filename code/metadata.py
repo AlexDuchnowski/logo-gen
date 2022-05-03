85f50298ab0f13a53afcec49a74a3d864be6d9d2
yuqinan <78807720+yuqinan@users.noreply.github.com>
2022-05-03 11:49:41 -0400

    Update metadata.py

--- code/metadata.py
+++ code/metadata.py
@@ -2,9 +2,10 @@ import math
 import numpy as np
 import os
 import pickle as cPickle
+import pprint as pp
 
 with open("../LLD-logo_metadata.pkl", 'rb') as f:
     metadata = cPickle.load(f, encoding='latin1')
 
 print(type(metadata["060608"]));
-print(metadata["060608"]["user_object"]);
+pp.pprint(metadata["060608"]["user_object"]);
