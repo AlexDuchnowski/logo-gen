4fd9ac51cd18cd991a8e20d7582718cb232499c3
h2323547425 <77903466+h2323547425@users.noreply.github.com>
2022-05-03 12:37:35 -0400

    Update metadata.py

--- code/metadata.py
+++ code/metadata.py
@@ -11,7 +11,7 @@ with open("../LLD-logo_metadata.pkl", 'rb') as f:
     metadata = cPickle.load(f, encoding='latin1')
 
 print(list(metadata.keys())[:100])
-description = vars(metadata["google"]["user_object"])["description"]
+description = vars(metadata["dna"]["user_object"])["description"]
 # words = description.split(" ");
 # #words = [word if word.isalpha() for word in words]
 # for word in words:
