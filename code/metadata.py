cae9b1f179a282863ad09551b2b096b6ffe26bca
h2323547425 <77903466+h2323547425@users.noreply.github.com>
2022-05-03 12:36:22 -0400

    Update metadata.py

--- code/metadata.py
+++ code/metadata.py
@@ -10,7 +10,8 @@ pp = pprint.PrettyPrinter(indent=2)
 with open("../LLD-logo_metadata.pkl", 'rb') as f:
     metadata = cPickle.load(f, encoding='latin1')
 
-description = vars(metadata[:20]["user_object"])["description"]
+print(list(metadata.keys())[:100])
+description = vars(metadata["google"]["user_object"])["description"]
 # words = description.split(" ");
 # #words = [word if word.isalpha() for word in words]
 # for word in words:
