c7b9d881b17eb062cb5c206f8e88e0c54567b357
h2323547425 <77903466+h2323547425@users.noreply.github.com>
2022-05-03 12:34:53 -0400

    Update metadata.py

--- code/metadata.py
+++ code/metadata.py
@@ -10,12 +10,12 @@ pp = pprint.PrettyPrinter(indent=2)
 with open("../LLD-logo_metadata.pkl", 'rb') as f:
     metadata = cPickle.load(f, encoding='latin1')
 
-description = vars(metadata["beautiful-sail"]["user_object"])["description"]
-words = description.split(" ");
-#words = [word if word.isalpha() for word in words]
-for word in words:
-    if word.isalpha():
-        print(word)
-    else:
-        print("no")
-pp.pprint(words)
+description = vars(metadata[:20]["user_object"])["description"]
+# words = description.split(" ");
+# #words = [word if word.isalpha() for word in words]
+# for word in words:
+#     if word.isalpha():
+#         print(word)
+#     else:
+#         print("no")
+pp.pprint(description)
