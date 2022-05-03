6c3a14bb28df8427022f6511e8e3089d4162e250
h2323547425 <77903466+h2323547425@users.noreply.github.com>
2022-05-03 12:26:40 -0400

    Update metadata.py

--- code/metadata.py
+++ code/metadata.py
@@ -10,14 +10,14 @@ pp = pprint.PrettyPrinter(indent=2)
 with open("../LLD-logo_metadata.pkl", 'rb') as f:
     metadata = cPickle.load(f, encoding='latin1')
 
-myObject = metadata["beautiful-sail"]["user_object"]
-myStr = str(myObject)
-match = re.search(myStr, "description.+friends_count")
-if match:
-  print('found', match.group()) ## 'found word:cat'
-else:
-  print('did not find')
-#res = json.loads(myStr)
+description = vars(metadata["beautiful-sail"]["user_object"])["description"]
+# myStr = str(myObject)
+# match = re.search(myStr, "description.+friends_count")
+# if match:
+#   print('found', match.group()) ## 'found word:cat'
+# else:
+#   print('did not find')
+# #res = json.loads(myStr)
 
 # pp.pprint(res)
 # pp.pprint(res["description"])
