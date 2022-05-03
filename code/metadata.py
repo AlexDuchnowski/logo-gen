d2016de401722fa21ae6f52ceb321a1f5ecd6e01
h2323547425 <77903466+h2323547425@users.noreply.github.com>
2022-05-03 11:39:34 -0400

    Update metadata.py

--- code/metadata.py
+++ code/metadata.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import pickle as cPickle
 
-with open("LLD-logo_metadata.pkl", 'rb') as f:
+with open("../LLD-logo_metadata.pkl", 'rb') as f:
     metadata = cPickle.load(f, encoding='latin1')
 
 print(list(metadata.keys())[:20])
