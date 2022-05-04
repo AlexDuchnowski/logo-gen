035e5dbda7a08561d3d10ba4fd8fe7e2da4e819f
Sidharth Anand <sid32502@gmail.com>
2022-05-03 21:52:39 -0400

    progress on preprocessing

--- code/preprocess.py
+++ code/preprocess.py
@@ -5,77 +5,72 @@ import matplotlib.pyplot as plt
 import json
 
 
-def data_generator(data_file_path, batch_size):
+WINDOW_SIZE = 30
+
+def make_generator(data_file_path, batch_size):
+    """
+    Returns a function that, when called without arguments, returns a generator on batches of data.
+
+    :param data_file_path: file path to the hdf5 file containing the data
+    :param batch_size: batch size for splitting up the model inputs
+    :return: the final generator yields (images, descriptions, names)
+    """
     hdf5_file = h5py.File(data_file_path, 'r')
+    def get_epoch():
+        for i in range(0, len(hdf5_file['data']), batch_size):
+            if i + batch_size >= len(hdf5_file['data']):
+                break
 
-def get_data(filepath):
-    # return a generator that can be iterated through to get subsequent batches
-    # NHWC transpose, resizing, and normalization of pixel values for images
+            # transpose from NCHW to NHWC
+            images = tf.cast(tf.transpose(hdf5_file['data'][i:i+batch_size], [0, 2, 3, 1]), tf.int32)
+            users = [json.loads(user_json) for user_json in hdf5_file['meta_data']['twitter']['user_objects'][i:i+batch_size]]
+            descriptions = [user['description'] for user in users]
+            names = [user['name'] for user in users]
+
+            images = process_images(images)
+            descriptions = process_descriptions(descriptions)
+            names = process_names(names)
+
+            yield images, descriptions, names
+    return get_epoch
+
+def process_images(images):
+    # resizing and normalization of pixel values for images
+    images = tf.image.resize(images, [64, 64])
+    images = tf.cast(images, tf.float32) / 255
+    return images
+
+def process_descriptions(descriptions):
     # create vocab for descriptions and replace words with corresponding ids (maybe UNK some words)
+    # REPLACE WITH REAL PREPROCESSING
+    padded_descriptions = []
+    for desc in descriptions:
+        # TRANSLATE HERE - ignore this placeholder stuff
+        lengths = [len(token) for token in desc.split()]
+        if len(lengths) < WINDOW_SIZE:
+            lengths.extend([0 for _ in range(WINDOW_SIZE - len(lengths))])
+        else:
+            lengths = lengths[:WINDOW_SIZE]
+        padded_descriptions.append(lengths)
+    return tf.convert_to_tensor(padded_descriptions)
+
+def process_names(names):
     # convert company names to ASCII
+    padded_names = []
+    for name in names:
+        ascii = list([ord(char) / 255 if char.isascii() else 0 for char in name])
+        if len(ascii) < WINDOW_SIZE:
+            ascii.extend([0 for _ in range(WINDOW_SIZE - len(ascii))])
+        else:
+            ascii = ascii[:WINDOW_SIZE]
+        padded_names.append(ascii)
+    return tf.convert_to_tensor(padded_names)
 
-    # metadata = unpickle(metadata_file_path)
-    hdf5_file = h5py.File(filepath, 'r')
-    # NCHW -> NHWC
-    # print(hdf5_file['meta_data']['names'][:5])
-    # print(hdf5_file['meta_data']['twitter']['ids'][:5])
-    # users = hdf5_file['meta_data']['twitter']['user_objects'][:5]
-    # print([json.loads(user)['description'] for user in users])
-    # print([json.loads(user)['name'] for user in users])
-    print(len(hdf5_file['data']))
-    # images = tf.transpose(hdf5_file['data'][:5], [0, 2, 3, 1])
-    # descriptions = hdf5_file['meta_data/user_object']['user_object'][:5]
-    # print(descriptions)
-    # # descriptions = tf.convert_to_tensor([vars(user["user_object"])["description"] for user in metadata.values()])[:5]
-    # # names = tf.convert_to_tensor([vars(user["user_object"])["name"] for user in metadata.values()])[:5]
-    # return images, descriptions #, names
+gen = make_generator('LLD-logo.hdf5', 128)()
+images, descriptions, names = next(gen)
+print(images)
+print(descriptions)
+print(names)
 
-get_data('LLD-logo.hdf5')
-# print(images.shape)
-# print(descriptions.shape)
-# print(names.shape)
-
-# # code adapted from: https://data.vision.ee.ethz.ch/sagea/lld/
-
-# def load_icon_data(data_path, pattern='LLD*.pkl', single_file=None):
-#     """Loads icon data from pickle files in directory specified in data_path
-#         pattern:        file name pattern to search for ['LLD_favicon_data*.pkl']
-#         single_file:    If not None, only the file with the specified number (modulo total number of files) 
-#                         is loaded [None]
-#         Returns the numpy arrays of shape (num_icons, 32, 32, 3) of dtype uint8
-
-# 	Example use with PIL:
-# 	icons = load_icon_data(data_path)
-# 	img = PIL.Image.fromarray(icons[0])
-# 	img.show()"""
-#     files = glob(os.path.join(data_path, pattern))
-#     files.sort()
-#     if single_file is None:
-#         with open(files[0], 'rb') as f:
-#             # used latin1 encoding to resolve unpickling error
-#             # https://stackoverflow.com/questions/11305790/pickle-incompatibility-of-numpy-arrays-between-python-2-and-3
-#             icons = cPickle.load(f, encoding='latin1')
-#         if len(files) > 1:
-#             for file in files[1:]:
-#                 with open(file, 'rb') as f:
-#                     icons_loaded = cPickle.load(f, encoding='latin1')
-#                 icons = np.concatenate((icons, icons_loaded))
-#     else:
-#         with open(files[single_file % len(files)], 'rb') as f:
-#             icons = cPickle.load(f, encoding='latin1')
-#     return icons
-
-# def save_icon_data(icons, data_path, package_size=100000):
-#     if not os.path.exists(data_path):
-#         os.makedirs(data_path)
-#     num_packages = int(math.ceil(len(icons) / package_size))
-#     num_len = len(str(num_packages))
-#     for p in range(num_packages):
-#         with open(os.path.join(data_path, 'LLD-icon_data_' + str(p).zfill(num_len) + '.pkl'), 'wb') as f:
-#             cPickle.dump(icons[p*package_size:(p+1)*package_size], f, protocol=cPickle.HIGHEST_PROTOCOL)
-
-# icons = load_icon_data('LLD-icon', single_file=1)
-# for i in range(20):
-#     plt.imshow(icons[i])
-#     plt.show()
-# # print(np.shape(icons))
+plt.imshow(images[0])
+plt.savefig('img.png')
