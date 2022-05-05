2a5fdc48bb2a182e8eda3d6edfbc79bab98b5454
yuqinan <qinan_yu@brown.edu>
2022-05-05 15:12:31 -0400

    crreunt

--- code/preprocess.py
+++ code/preprocess.py
@@ -6,6 +6,7 @@ import json
 
 import googletrans 
 from gensim.models import Word2Vec
+from gensim.test.utils import common_texts
 import logging
 model = Word2Vec(sentences=common_texts, vector_size=100, window=5, min_count=1, workers=4)
 model.save("word2vec.model")
