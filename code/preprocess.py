0696dabae3b3d3ad73b6df13faea9379711f173f
yuqinan <qinan_yu@brown.edu>
2022-05-05 15:05:47 -0400

    message

--- code/preprocess.py
+++ code/preprocess.py
@@ -4,7 +4,12 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import json
 import googletrans 
-
+from gensim.models import word2vec
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+sentences = word2vec.Text8Corpus('text8')
+# size here should be the embedding size
+model = word2vec.Word2Vec(sentences, size=200)
 WINDOW_SIZE = 30
 
 def make_input_generator(data_file_path, batch_size, epochs):
